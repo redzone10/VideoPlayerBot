3d33bee0f8e46bd6045f43e71f2b40b5cff19148
Raks <87678334+redzone10@users.noreply.github.com>
2021-09-11 16:41:36 +0700

    Update tr.py

--- plugins/tr.py
+++ plugins/tr.py
@@ -1,8 +1,8 @@
 import os
 from config import ASSISTANT_NAME
 
-HELP_TEXT =f"**SETTINGS** \n\n`1) Add this Bot to your Group and Make it Admin` \n2) **Add** @{ASSISTANT_NAME} **to your Group** \n\n**COMMANDS** \n\n- `/vplay` **: Reply to Video or File That You Want To stream In Vc.** \n- `/stop`  **: Stop the stream**"
+HELP_TEXT =f"**SETTINGS** \n\n`➢ ᴛᴀᴍʙᴀʜᴋᴀɴ 𝙆𝙂𝙎𝙏𝙍𝙀𝘼𝙈𝙑𝙄𝘿𝙀𝙊 ᴋᴇ ᴅᴀʟᴀᴍ ɢʀᴏᴜᴘ ᴅᴀɴ ᴊᴀᴅɪᴋᴀɴ ᴀᴅᴍɪɴ` \n➢  **ᴀᴅᴅ** @{ASSISTANT_NAME} **ᴋᴇɢʀᴏᴜᴘ ᴀɴᴅᴀ** \n\n**ᴘᴇʀɪɴᴛᴀʜ** \n\n• `/vplay` **: ʀᴇᴘʟʏ ᴠɪᴅᴇᴏ ᴀᴛᴀᴜ ᴋɪʀɪᴍ ʟɪɴᴋ ᴅᴀʀɪ ʏᴏᴜᴛᴜʙᴇ.** \n- `/stop`  **: ᴍᴇᴍʙᴇʀʜᴇɴᴛɪᴋᴀɴ sᴛʀᴇᴀᴍ**"
 
-START_TEXT = '👋 `Hey My Name Is` **Video Player Bot** \n\n`Lets Enjoy Cinematic View  of Group Video Player With Your Friends ❤️😊`'
+START_TEXT = '👋 𝙃𝙀𝙇𝙇𝙊 𝙎𝘼𝙔𝘼 𝙆𝙂𝙎𝙏𝙍𝙀𝘼𝙈𝙑𝙄𝘿𝙀𝙊\n\n💡𝙎𝙖𝙮𝙖 𝙖𝙠𝙖𝙣 𝙢𝙚𝙢𝙗𝙖𝙣𝙩𝙪 𝙢𝙚𝙧𝙖𝙢𝙖𝙞𝙠𝙖𝙣 𝙜𝙧𝙪𝙥 𝙠𝙖𝙡𝙞𝙖𝙣 𝙙𝙚𝙣𝙜𝙖𝙣 𝙘𝙖𝙧𝙖 𝙢𝙚𝙢𝙪𝙩𝙖𝙧𝙠𝙖𝙣 𝙫𝙞𝙙𝙚𝙤 𝙙𝙞 𝙤𝙗𝙧𝙤𝙡𝙖𝙣 𝙨𝙪𝙖𝙧𝙖 𝙨𝙚𝙨𝙪𝙖𝙞 𝙮𝙖𝙣𝙜 𝙠𝙖𝙡𝙞𝙖𝙣 𝙢𝙞𝙣𝙩𝙖\n❓𝙆𝙇𝙄𝘾𝙆 𝘿𝙄𝙎𝙄𝙉𝙄 𝙐𝙣𝙩𝙪𝙠 𝙢𝙚𝙣𝙖𝙢𝙗𝙖𝙝𝙠𝙖𝙣 𝙨𝙖𝙮𝙖 𝙠𝙚𝙜𝙧𝙪𝙥.'
 
-ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 This bot was created to stream video in telegram group video chats using several methods from WebRTC.` \n\n`👨‍🔧 Powered by PyTgcalls the Async client API for the Telegram Group Calls, and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
+ABOUT_TEXT = '`𝙹𝚊𝚗𝚐𝚊𝚗 𝙻𝚞𝚙𝚊 𝙹𝚘𝚒𝚗 𝚈𝚊𝚗𝚐 𝙳𝚒𝚋𝚊𝚠𝚊𝚑 𝚢𝚊!!`'
